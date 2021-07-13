004cdd17fff7da27c60135b9cc196e840339b367
Shagufta2 <87363881+Shagufta2@users.noreply.github.com>
2021-07-13 21:18:14 +0530

    Add files via upload

---
+++ AdditionNumbers.java
@@ -0,0 +1,15 @@
+package labexercises;
+
+import java.util.Scanner;
+
+public class AdditionNumbers {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 230;
+		
+		int addition = x + y;
+		
+		System.out.println(addition);
+		
+	} 
+}
